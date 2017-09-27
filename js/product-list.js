579aa0d2a9302ec2108545ae5e7b4887d609f863
cognitive-cake <nd.chernikov@gmail.com>
2017-09-27 22:28:53 +0300

    Начал работать над интерактивностью списка заказов

--- js/product-list.js
+++ js/product-list.js
@@ -1,1 +1,12 @@
 'use strict';
+
+(function () {
+  var orderForm = document.querySelector('.order-list');
+  var allOrders = orderForm.querySelectorAll('.single-product');
+  
+  orderForm.addEventListener('click', onAmountClick);
+  
+  function onAmountClick(event) {
+    var currentTarget = event.target;
+  }
+})();
