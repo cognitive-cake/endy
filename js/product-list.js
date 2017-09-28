9d70d5be4b0d87cf16920badd00ea4d940aae0a1
cognitive-cake <nd.chernikov@gmail.com>
2017-09-28 19:12:26 +0300

    Написал базовый функционал счётчика товаров

--- js/product-list.js
+++ js/product-list.js
@@ -1,12 +1,43 @@
 'use strict';
 
 (function () {
-  var orderForm = document.querySelector('.order-list');
-  var allOrders = orderForm.querySelectorAll('.single-product');
-  
-  orderForm.addEventListener('click', onAmountClick);
-  
-  function onAmountClick(event) {
-    var currentTarget = event.target;
+  var PARAMETERS = {
+    formClass: 'order-list',
+    singlePositionClass: 'single-product',
+    buttonClass: 'amount-controls__btn',
+    buttonDecClass: 'amount-controls__btn-dec',
+    buttonIncClass: 'amount-controls__btn-inc',
+    disabledClass: 'disabled',
+    valueClass: 'amount-controls__value',
+    radixForValue: 10,
+    decrementStep: 1,
+    incrementStep: 1
+  };
+
+  var orderForm = document.querySelector('.' + PARAMETERS.formClass);
+  var allOrders = orderForm.querySelectorAll('.' + PARAMETERS.singlePositionClass);
+
+  function onAmountControlsClick(event) {
+    var clickTarget = event.target;
+    // debugger;
+    while (clickTarget !== orderForm) {
+      if (clickTarget.classList.contains(PARAMETERS.buttonClass) && !clickTarget.classList.contains(PARAMETERS.disabledClass)) {
+        changeAmount(clickTarget);
+        return;
+      }
+      clickTarget = clickTarget.parentElement;
+    }
   }
+
+  function changeAmount(clickedBtn) {
+    var counter = clickedBtn.parentElement.querySelector('.' + PARAMETERS.valueClass);
+
+    if (clickedBtn.classList.contains(PARAMETERS.buttonDecClass)) {
+      counter.value = parseInt(counter.value, PARAMETERS.radixForValue) - PARAMETERS.decrementStep;
+    } else if (clickedBtn.classList.contains(PARAMETERS.buttonIncClass)) {
+      counter.value = parseInt(counter.value, PARAMETERS.radixForValue) + PARAMETERS.incrementStep;
+    }
+  }
+
+  orderForm.addEventListener('click', onAmountControlsClick);
 })();
