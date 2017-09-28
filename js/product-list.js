c0e26ba0fbb6fef7936b78d577969678eb609456
cognitive-cake <nd.chernikov@gmail.com>
2017-09-28 21:29:59 +0300

    Добавил деактивацию кнопки, если достигнуто минимальное кол-во товара

--- js/product-list.js
+++ js/product-list.js
@@ -3,7 +3,7 @@
 (function () {
   var PARAMETERS = {
     formClass: 'order-list',
-    singlePositionClass: 'single-product',
+    singlePositionClass: 'single-order',
     buttonClass: 'amount-controls__btn',
     buttonDecClass: 'amount-controls__btn-dec',
     buttonIncClass: 'amount-controls__btn-inc',
@@ -11,12 +11,15 @@
     valueClass: 'amount-controls__value',
     radixForValue: 10,
     decrementStep: 1,
-    incrementStep: 1
+    incrementStep: 1,
+    minValue: 1
   };
 
   var orderForm = document.querySelector('.' + PARAMETERS.formClass);
-  var allOrders = orderForm.querySelectorAll('.' + PARAMETERS.singlePositionClass);
 
+  // --- Обработчики событий ---
+
+  // Отлавливаем клик на кнопках изменения кол-ва товара
   function onAmountControlsClick(event) {
     var clickTarget = event.target;
     // debugger;
@@ -29,6 +32,9 @@
     }
   }
 
+  // ^^^ Обработчики событий ^^^
+
+  // Изменение количества товара
   function changeAmount(clickedBtn) {
     var counter = clickedBtn.parentElement.querySelector('.' + PARAMETERS.valueClass);
 
@@ -39,5 +45,35 @@
     }
   }
 
+  // Функция возвращает true, если указано минимальное количество товара
+  function hasMinAmount(singleProductCard) {
+    var valueField = singleProductCard.querySelector('.' + PARAMETERS.valueClass);
+    return parseInt(valueField.value, PARAMETERS.radixForValue) === PARAMETERS.minValue;
+  }
+
+  // Делает кнопку неактивной
+  function disableButton(singleProductCard) {
+    var decBtn = singleProductCard.querySelector('.' + PARAMETERS.buttonDecClass);
+    decBtn.classList.add(PARAMETERS.disabledClass);
+    decBtn.setAttribute('disabled', '');
+  }
+
+  // Делает кнопку активной
+  function enableButton(singleProductCard) {
+    var decBtn = singleProductCard.querySelector('.' + PARAMETERS.buttonDecClass);
+    decBtn.classList.remove(PARAMETERS.disabledClass);
+    decBtn.removeAttribute('disabled');
+  }
+
+
+  // Выполнение скрипта
+  window.addEventListener('load', function () {
+    var allOrders = orderForm.querySelectorAll('.' + PARAMETERS.singlePositionClass);
+    allOrders.forEach(function (element) {
+      if (hasMinAmount(element)) {
+        disableButton(element);
+      }
+    });
+  });
   orderForm.addEventListener('click', onAmountControlsClick);
 })();
