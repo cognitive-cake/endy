4bc7d0a1bef5a44fd51906715d3107a6ada386ad
Nick-ch <nd.chernikov@gmail.com>
2017-09-30 17:38:24 +0300

    Начал работу над логикой прогресс-бара

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -1,1 +1,12 @@
 'use strict';
+
+(function () {
+  var PARAMETERS = {
+    progressFrameClass: 'progress',
+    progressVisualizationClass: 'progress__visualization',
+    progressBarFillerClass: 'progress-bar__filler',
+    progressValueClass: 'progress__value'
+  };
+
+  var progressFrame = document.querySelector('.' + PARAMETERS.progressFrameClass);
+})();
