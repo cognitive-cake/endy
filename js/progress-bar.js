22a8d918178595924a142fe44d34cd2d1208d0ed
Nick-ch <nd.chernikov@gmail.com>
2017-09-30 21:31:28 +0300

    Написал базовою механику работы прогресс-бара

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -4,9 +4,50 @@
   var PARAMETERS = {
     progressFrameClass: 'progress',
     progressVisualizationClass: 'progress__visualization',
+    progressBarClass: 'progress-bar',
     progressBarFillerClass: 'progress-bar__filler',
-    progressValueClass: 'progress__value'
+    progressValueClass: 'progress__value',
+    radixForValue: 10,
+    widthUnits: 'px',
+    valueUnits: '%'
   };
 
   var progressFrame = document.querySelector('.' + PARAMETERS.progressFrameClass);
+  var progressBar = progressFrame.querySelector('.' + PARAMETERS.progressBarClass);
+  var barFiller = progressFrame.querySelector('.' + PARAMETERS.progressBarFillerClass);
+  var progressValue = progressFrame.querySelector('.' + PARAMETERS.progressValueClass);
+
+  var oldValue;
+
+  function onDataAttributeChange() {
+    var newValue = getValueFromDataAttribute();
+    if (newValue !== oldValue) {
+      setFillerWidth(newValue);
+      setNewDisplayedValue(newValue);
+      setPositionOfDisplayedValue(newValue);
+
+      oldValue = newValue;
+    }
+  }
+
+  function getValueFromDataAttribute() {
+    var rawValue = progressValue.dataset.progress;
+    var value = parseInt(rawValue, PARAMETERS.radixForValue);
+    return value;
+  }
+
+  function setFillerWidth(value) {
+    barFiller.style.width = progressBar.offsetWidth * value / 100 + PARAMETERS.widthUnits;
+  }
+
+  function setNewDisplayedValue(value) {
+    progressValue.textContent = value + PARAMETERS.valueUnits;
+  }
+
+  function setPositionOfDisplayedValue(value) {
+    progressValue.style.left = value + PARAMETERS.valueUnits;
+  }
+
+  // Выполнение скрипта
+  onDataAttributeChange();
 })();
