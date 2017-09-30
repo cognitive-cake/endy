4041ad119e86b6d6d55a4de38be8d6ff60c1885b
Nick-ch <nd.chernikov@gmail.com>
2017-09-30 23:51:34 +0300

    Написал расчёт позиции для цифрового обозначения прогресса

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -8,17 +8,22 @@
     progressBarFillerClass: 'progress-bar__filler',
     progressValueClass: 'progress__value',
     radixForValue: 10,
-    widthUnits: '%',
-    valueUnits: '%'
+    fillerUnits: '%',
+    transformUnits: 'px',
+    valueUnits: '%',
+    valuePositionUnits: 'px'
   };
 
   var progressFrame = document.querySelector('.' + PARAMETERS.progressFrameClass);
-  var progressBar = progressFrame.querySelector('.' + PARAMETERS.progressBarClass);
   var barFiller = progressFrame.querySelector('.' + PARAMETERS.progressBarFillerClass);
   var progressValue = progressFrame.querySelector('.' + PARAMETERS.progressValueClass);
+  var progressVisualization = progressFrame.querySelector('.' + PARAMETERS.progressVisualizationClass);
 
   var oldValue;
 
+  // --- Обработчики событий ---
+
+  // Если изменился атрибут
   function onDataAttributeChange() {
     var newValue = getValueFromDataAttribute();
     if (newValue !== oldValue) {
@@ -30,22 +35,48 @@
     }
   }
 
+  // ^^^ Обработчики событий ^^^
+
+  // Возвращает текущее значение атрибута data-progress
   function getValueFromDataAttribute() {
     var rawValue = progressValue.dataset.progress;
     var value = parseInt(rawValue, PARAMETERS.radixForValue);
     return value;
   }
 
+  // Устанавливает ширину уровня заполненности
   function setFillerWidth(value) {
-    barFiller.style.width = value + PARAMETERS.widthUnits;
+    barFiller.style.width = value + PARAMETERS.fillerUnits;
   }
 
+  // Прописывает новую величину прогресс-бара в DOM-элементе
   function setNewDisplayedValue(value) {
     progressValue.textContent = value + PARAMETERS.valueUnits;
   }
 
+  // Позиционирует DOM-элемент, отображающий текущую величину
   function setPositionOfDisplayedValue(value) {
-    progressValue.style.left = value + PARAMETERS.widthUnits;
+    var position = calculatePosition(value);
+    progressValue.style.left = position + PARAMETERS.valuePositionUnits;
+  }
+
+  // Рассчитывает позицию для DOM-элемента progress__value
+  function calculatePosition(value) {
+    // Комментарий, объясняющий, почему не написана более очевидная реализация позиционирования через проценты.
+    // В ТЗ этого сказано не было, но на макете видно, что при значении '100%' цифра не выходит за границы прогресс-бара, а при значении '40%' - центр элемента находится ровно под границей уровня заполненности.
+    // В первом варианте позиция задавалась вот так:
+    // var cssString = 'transform: translateX(-50%);' + 'left: ' + value + '%;';
+    // progressValue.style.cssText = cssString;
+    // Т.е. через проценты и центрование с помощью transform. От этого варианта пришлось отказаться, т.к. при такой реализации неполучается отследить момент, когда отображаемое значение прогресс-бара начинает вылезать за пределы родителя.
+    var valueBlockWidth = progressValue.offsetWidth;
+    var parentWidth = progressVisualization.offsetWidth;
+    var position = parentWidth * value / 100 - valueBlockWidth / 2;
+    if (position < 0) {
+      position = 0;
+    } else if (position + valueBlockWidth > parentWidth) {
+      position = parentWidth - valueBlockWidth;
+    }
+    return position;
   }
 
   // Выполнение скрипта
