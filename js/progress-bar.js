0c156a307f9f55aae989ed4683291c4ea71657d1
Nick-ch <nd.chernikov@gmail.com>
2017-10-01 20:27:15 +0300

    Переписал логику прогресс-бара для работы с несколькими элементами

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -2,80 +2,69 @@
 
 (function () {
   var PARAMETERS = {
-    progressFrameClass: 'progress',
-    progressVisualizationClass: 'progress__visualization',
-    progressBarClass: 'progress-bar',
+    progressFrameClass: 'order-progress',
     progressBarFillerClass: 'progress-bar__filler',
     progressValueClass: 'progress__value',
     radixForValue: 10,
     fillerUnits: '%',
-    transformUnits: 'px',
     valueUnits: '%',
-    valuePositionUnits: 'px'
+    valuePositionUnits: 'px',
+    dataAttribute: 'data-progress'
   };
 
-  var progressFrame = document.querySelector('.' + PARAMETERS.progressFrameClass);
-  var barFiller = progressFrame.querySelector('.' + PARAMETERS.progressBarFillerClass);
-  var progressValue = progressFrame.querySelector('.' + PARAMETERS.progressValueClass);
-  var progressVisualization = progressFrame.querySelector('.' + PARAMETERS.progressVisualizationClass);
-
-  var oldValue;
+  var orderProgress = document.querySelector('.' + PARAMETERS.progressFrameClass);
+  var allValueElements = orderProgress.querySelectorAll('.' + PARAMETERS.progressValueClass);
 
   // Отслеживание изменения data-атрибутов
   var observer = new MutationObserver(onDataAttributeChange);
   var observerConfig = {
-    attributes: true
+    attributes: true,
+    attributeOldValue: true,
+    attributeFilter: [PARAMETERS.dataAttribute]
   };
 
   // --- Обработчики событий ---
 
   // Если изменился атрибут
-  function onDataAttributeChange() {
-    var newValue = getValueFromDataAttribute();
-    if (newValue !== oldValue) {
-      setFillerWidth(newValue);
-      setNewDisplayedValue(newValue);
-      setPositionOfDisplayedValue(newValue);
+  function onDataAttributeChange(arrayOfMutationRecords) {
+    var record = arrayOfMutationRecords[0];
+    var visualizationElement = record.target.parentElement;
+    var valueElement = record.target;
+    var newValue = getValueFromDataAttribute(valueElement);
 
-      oldValue = newValue;
+    if (newValue !== +record.oldValue) {
+      setFillerWidth(visualizationElement, newValue);
+      setNewDisplayedValue(valueElement, newValue);
+
+      var position = calculatePosition(valueElement, visualizationElement, newValue);
+      setPositionOfDisplayedValue(valueElement, position);
     }
   }
 
   // ^^^ Обработчики событий ^^^
 
   // Возвращает текущее значение атрибута data-progress
-  function getValueFromDataAttribute() {
-    var rawValue = progressValue.dataset.progress;
+  function getValueFromDataAttribute(element) {
+    var rawValue = element.dataset.progress;
     var value = parseInt(rawValue, PARAMETERS.radixForValue);
     return value;
   }
 
   // Устанавливает ширину уровня заполненности
-  function setFillerWidth(value) {
+  function setFillerWidth(element, value) {
+    var barFiller = element.querySelector('.' + PARAMETERS.progressBarFillerClass);
     barFiller.style.width = value + PARAMETERS.fillerUnits;
   }
 
   // Прописывает новую величину прогресс-бара в DOM-элементе
-  function setNewDisplayedValue(value) {
-    progressValue.textContent = value + PARAMETERS.valueUnits;
-  }
-
-  // Позиционирует DOM-элемент, отображающий текущую величину
-  function setPositionOfDisplayedValue(value) {
-    var position = calculatePosition(value);
-    progressValue.style.left = position + PARAMETERS.valuePositionUnits;
+  function setNewDisplayedValue(element, value) {
+    element.textContent = value + PARAMETERS.valueUnits;
   }
 
   // Рассчитывает позицию для DOM-элемента progress__value
-  function calculatePosition(value) {
-    // Комментарий, объясняющий, почему не написана более очевидная реализация позиционирования через проценты.
-    // В ТЗ этого сказано не было, но на макете видно, что при значении '100%' цифра не выходит за границы прогресс-бара, а при значении '40%' - центр элемента находится ровно под границей уровня заполненности.
-    // В первом варианте позиция задавалась вот так:
-    // var cssString = 'transform: translateX(-50%);' + 'left: ' + value + '%;';
-    // progressValue.style.cssText = cssString;
-    // Т.е. через проценты и центрование с помощью transform. От этого варианта пришлось отказаться, т.к. при такой реализации неполучается отследить момент, когда отображаемое значение прогресс-бара начинает вылезать за пределы родителя.
-    var valueBlockWidth = progressValue.offsetWidth;
-    var parentWidth = progressVisualization.offsetWidth;
+  function calculatePosition(valueElement, visualizationElement, value) {
+    var valueBlockWidth = valueElement.offsetWidth;
+    var parentWidth = visualizationElement.offsetWidth;
     var position = parentWidth * value / 100 - valueBlockWidth / 2;
     if (position < 0) {
       position = 0;
@@ -85,7 +74,13 @@
     return position;
   }
 
+  // Позиционирует DOM-элемент, отображающий текущую величину
+  function setPositionOfDisplayedValue(element, position) {
+    element.style.left = position + PARAMETERS.valuePositionUnits;
+  }
+
   // Выполнение скрипта
-  onDataAttributeChange();
-  observer.observe(progressValue, observerConfig);
+  allValueElements.forEach(function (element) {
+    observer.observe(element, observerConfig);
+  });
 })();
