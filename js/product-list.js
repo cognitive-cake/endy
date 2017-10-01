621bd264233ebe0defff036d25f4d5cc46c74aaa
Nick-ch <nd.chernikov@gmail.com>
2017-10-01 23:57:06 +0300

    Добавил больше функций в product-list.js для лучшей читаемости

--- js/product-list.js
+++ js/product-list.js
@@ -30,19 +30,20 @@
   // Клик на кнопках изменения кол-ва товара
   function onAmountControlsClick(event) {
     var clickTarget = event.target;
+
     while (clickTarget !== orderForm) {
+
       if (clickTarget.classList.contains(PARAMETERS.buttonClass) && !clickTarget.classList.contains(PARAMETERS.disabledClass)) {
+
         var amountControls = clickTarget.parentElement;
         var decButton = amountControls.querySelector('.' + PARAMETERS.buttonDecClass);
 
         changeAmount(clickTarget);
-        if (hasMinAmount(amountControls)) {
-          disableButton(decButton);
-        } else if (decButton.classList.contains(PARAMETERS.disabledClass)) {
-          enableButton(decButton);
-        }
+        checkForMinValue(amountControls, decButton);
         return;
+
       }
+
       clickTarget = clickTarget.parentElement;
     }
   }
@@ -54,12 +55,10 @@
       if (clickTarget.classList.contains(PARAMETERS.removeButtonClass)) {
         var parentElement = clickTarget.parentElement;
         if (clickTarget.classList.contains(PARAMETERS.returnButtonClass)) {
-          clickTarget.classList.remove(PARAMETERS.returnButtonClass);
-          clickTarget.textContent = PARAMETERS.wordDelete;
+          switchButtonToRemove(clickTarget);
           enablePosition(parentElement);
         } else {
-          clickTarget.classList.add(PARAMETERS.returnButtonClass);
-          clickTarget.textContent = PARAMETERS.wordReturn;
+          switchButtonToReturn(clickTarget);
           disablePosition(parentElement);
         }
         return;
@@ -81,22 +80,53 @@
     }
   }
 
+  // Проверка на минимальное кол-во товара и работа с кнопкой "-"
+  function checkForMinValue(amountControls, decButton) {
+    if (hasMinAmount(amountControls)) {
+      disableButton(decButton);
+    } else if (decButton.classList.contains(PARAMETERS.disabledClass)) {
+      enableButton(decButton);
+    }
+  }
+
   // Функция возвращает true, если указано минимальное количество товара
   function hasMinAmount(amountControls) {
     var valueField = amountControls.querySelector('.' + PARAMETERS.valueClass);
-    return parseInt(valueField.value, PARAMETERS.radixForValue) === PARAMETERS.minValue;
+    return parseInt(valueField.value, PARAMETERS.radixForValue) <= PARAMETERS.minValue;
   }
 
-  // Делает кнопку недоступной и неактивной
+  // Делает кнопку декремента недоступной и неактивной
   function disableButton(decButton) {
     decButton.classList.add(PARAMETERS.disabledClass);
     decButton.setAttribute(PARAMETERS.disabledAttribute, '');
   }
 
-  // Делает кнопку доступной и активной
+  // Делает кнопку декремента доступной и активной
   function enableButton(decButton) {
     decButton.classList.remove(PARAMETERS.disabledClass);
     decButton.removeAttribute(PARAMETERS.disabledAttribute);
+  }
+
+  // Изменение кнопки с "Удалить" на "Вернуть"
+  function switchButtonToReturn(clickTarget) {
+    clickTarget.classList.add(PARAMETERS.returnButtonClass);
+    clickTarget.textContent = PARAMETERS.wordReturn;
+  }
+
+  // Изменение кнопки с "Вернуть" на "Удалить"
+  function switchButtonToRemove(clickTarget) {
+    clickTarget.classList.remove(PARAMETERS.returnButtonClass);
+    clickTarget.textContent = PARAMETERS.wordDelete;
+  }
+
+  // Активация товарной позиции
+  function enablePosition(parentElement) {
+    var elements = parentElement.children;
+    for (var i = 0; i < elements.length; i++) {
+      var singleNode = elements[i];
+      singleNode.classList.remove(PARAMETERS.disabledClass);
+      singleNode.removeAttribute(PARAMETERS.disabledAttribute);
+    }
   }
 
   // Деактивация товарной позиции
@@ -110,16 +140,6 @@
       if (singleNode.classList.contains(PARAMETERS.amountControlsClass)) {
         singleNode.setAttribute(PARAMETERS.disabledAttribute, '');
       }
-    }
-  }
-
-  // Активация товарной позиции
-  function enablePosition(parentElement) {
-    var elements = parentElement.children;
-    for (var i = 0; i < elements.length; i++) {
-      var singleNode = elements[i];
-      singleNode.classList.remove(PARAMETERS.disabledClass);
-      singleNode.removeAttribute(PARAMETERS.disabledAttribute);
     }
   }
 
